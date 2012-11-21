56e67ec0c39c92804279427ff20147883c451e22
Aaron Okano <adokano@ucdavis.edu>
2012-11-21 01:14:32 -0800

    Added stuff and did a little stuff

---
+++ p1.R
@@ -0,0 +1,27 @@
+# Construct matrix with mean area for each coordinate
+areaGrid <- function( data ) {
+  # For a general dataset of the same format, since I'm in the mood
+  origin <- min( data$X,data$Y )
+  maximum <- max( data$X,data$Y )
+  # Generate all possible coordinates. Use various transformations to create a
+  # list of the desired form
+  coords <- as.list(as.data.frame(t(expand.grid( origin:maximum, origin:maximum ))))
+  # Create map with mean areas
+  t(matrix( sapply( coords, 
+    function(x) mean( data[ which( data$X == x[1] & data$Y == x[2] ), ]$area ) ),
+           maximum, maximum ))
+}
+
+# Modification of above to show number of fires per location
+frequencyGrid <- function( data ) {
+  # For a general dataset of the same format, since I'm in the mood
+  origin <- min( data$X,data$Y )
+  maximum <- max( data$X,data$Y )
+  # Generate all possible coordinates. Use various transformations to create a
+  # list of the desired form
+  coords <- as.list(as.data.frame(t(expand.grid( origin:maximum, origin:maximum ))))
+  # Create map with mean areas
+  t(matrix( sapply( coords, 
+    function(x) length( data[ which( data$X == x[1] & data$Y == x[2] ), ]$area ) ),
+           maximum, maximum ))
+}
