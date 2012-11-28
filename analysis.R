287f97c5272a09c8833ae504f684e0f1dc19712f
Samuel Huang <samhuang91@gmail.com>
2012-11-28 12:37:14 -0800

    Formatted analysis

--- analysis.R
+++ analysis.R
@@ -1,11 +1,14 @@
-This is the analysis for the spread1 by using the method outlined in part 
-16.2.5
+#//////// SPREAD1 analysis ///////////
 
+parkinson <- read.csv('parkinsons.data', header=TRUE)
+y <- function(x,y,t) {1/(1+exp(-(x+y*t))) }
+zero <- which(parkinson$status == 0)
+one <- which(parkinson$status == 1)
 
-> g <- glm(parkinson$status ~ parkinson$spread1, family = binomial)
-> summary(g)
+g <- glm(parkinson$status ~ parkinson$spread1, family = binomial)
+summary(g)
 
-Call:
+str <- "Call:
 glm(formula = parkinson$status ~ parkinson$spread1, family = binomial)
 
 Deviance Residuals: 
@@ -25,67 +28,39 @@ Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’
 Residual deviance: 130.15  on 193  degrees of freedom
 AIC: 134.15
 
-Number of Fisher Scoring iterations: 6
+Number of Fisher Scoring iterations: 6"
 
-> mean(parkinson$spread1)
-[1] -5.684397
-> mean(parkinson$spread1[parkinson$status == 0])
-[1] -6.759264
-> mean(parkinson$spread1[parkinson$status == 1])
-[1] -5.33342
-> zero <- which(parkinson$status == 0)
-> one <- which(parkinson$status == 1)
-> 1/(1+exp(-(15.8608+2.3966*-6.759246)))
-[1] 0.416196 <--- ALMOST!
-> 1/(1+exp(-(15.8608+2.3966*-5.33342)))
-[1] 0.9560066 <---- GREAT VALUE!
+mean(parkinson$spread1[zero])
+#[1] -6.759264
+mean(parkinson$spread1[one])
+#[1] -5.33342
+y(15.8608,2.3966,-6.759246)
+#[1] 0.416196 <--- ALMOST!
+y(15.8608,2.3966,-5.33342)
+#[1] 0.9560066 <---- GREAT VALUE!
 
-//////// SPREAD2 analysis ///////////
+#//////// SPREAD2 analysis ///////////
 
+g <- glm(parkinson$status ~ parkinson$spread2, family = binomial)
+summary(g)
 
-> y <- function(x,y,t) {1/(1+exp(-(x+y*t))) }
-> g <- glm(parkinson$status ~ parkinson$spread2, family = binomial)
-> summary(g)
+mean(parkinson$spread2)
+#[1] 0.2265103
+mean(parkinson$spread2[zero])
+#[1] 0.160292
+mean(parkinson$spread2[one])
+#[1] 0.2481327
+y(-2.4283,17.5354, 0.160292)
+#[1] 0.5944722
+y(-2.4283,17.5354, 0.5944722)
+#[1] 0.9996633
 
-Call:
-glm(formula = parkinson$status ~ parkinson$spread2, family = binomial)
+#///////////// PPE ANALYSIS /////////////
 
-Deviance Residuals: 
-     Min        1Q    Median        3Q       Max  
--2.34813   0.09858   0.42402   0.70699   1.76286  
+g <- glm(parkinson$status ~ parkinson$PPE, family = binomial)
+summary(g)
 
-Coefficients:
-                  Estimate Std. Error z value Pr(>|z|)    
-(Intercept)        -2.4283     0.6196  -3.919 8.89e-05 ***
-parkinson$spread2  17.5354     3.1569   5.555 2.78e-08 ***
----
-Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1 
-
-(Dispersion parameter for binomial family taken to be 1)
-
-    Null deviance: 217.65  on 194  degrees of freedom
-Residual deviance: 170.40  on 193  degrees of freedom
-AIC: 174.4
-
-Number of Fisher Scoring iterations: 5
-
-> mean(parkinson$spread2)
-[1] 0.2265103
-> mean(parkinson$spread2[zero])
-[1] 0.160292
-> mean(parkinson$spread2[one])
-[1] 0.2481327
-> y(-2.4283,17.5354, 0.160292)
-[1] 0.5944722
-> y(-2.4283,17.5354, 0.5944722)
-[1] 0.9996633
-
-///////////// PPE ANALYSIS /////////////
-
-> g <- glm(parkinson$status ~ parkinson$PPE, family = binomial)
-> summary(g)
-
-Call:
+str <- "Call:
 glm(formula = parkinson$status ~ parkinson$PPE, family = binomial)
 
 Deviance Residuals: 
@@ -106,25 +81,26 @@ Residual deviance: 131.57  on 193  degrees of freedom
 AIC: 135.57
 
 Number of Fisher Scoring iterations: 6
+"
 
-> mean(parkinson$PPE)
-[1] 0.2065516
-> mean(parkinson$PPE[zero])
-[1] 0.1230171
-> mean(parkinson$PPE[one])
-[1] 0.2338282
-> y(-4.2214,32.2906,0.1230171)
-[1] 0.438044
-> y(-4.2214,32.2906,0.2338282)
-[1] 0.9654122
+mean(parkinson$PPE)
+#[1] 0.2065516
+mean(parkinson$PPE[zero])
+#[1] 0.1230171
+mean(parkinson$PPE[one])
+#[1] 0.2338282
+y(-4.2214,32.2906,0.1230171)
+#[1] 0.438044
+y(-4.2214,32.2906,0.2338282)
+#[1] 0.9654122
 
 
-/////////// NHR ANALYSIS //////////
+#/////////// NHR ANALYSIS //////////
 
-> g <- glm(parkinson$status ~ parkinson$NHR, family = binomial)
-> summary(g)
+g <- glm(parkinson$status ~ parkinson$NHR, family = binomial)
+summary(g)
 
-Call:
+str <- "Call:
 glm(formula = parkinson$status ~ parkinson$NHR, family = binomial)
 
 Deviance Residuals: 
@@ -145,14 +121,14 @@ Residual deviance: 202.94  on 193  degrees of freedom
 AIC: 206.94
 
 Number of Fisher Scoring iterations: 6
+"
 
-> mean(parkinson$NHR[zero])
-[1] 0.01148271
-> mean(parkinson$NHR[one])
-[1] 0.02921095
-> y(0.4717,39.8604,0.01148271)
-[1] 0.7169546
-> y(0.4717,39.8604,0.02921095)
-[1] 0.8369981
-
+mean(parkinson$NHR[zero])
+#[1] 0.01148271
+mean(parkinson$NHR[one])
+#[1] 0.02921095
+y(0.4717,39.8604,0.01148271)
+#[1] 0.7169546
+y(0.4717,39.8604,0.02921095)
+#[1] 0.8369981
 
